36f3a9d109b53e99f2bad0a6388173efff9b23a7
JULIAN HAZARD <hazardjulian74@gmail.com>
2025-01-06 10:22:14 +0100

    update fetch express

--- src/components/EmployeeCard.tsx
+++ src/components/EmployeeCard.tsx
@@ -1,29 +1,30 @@
 // src/components/EmployeeCard.tsx
 interface EmployeeCardProps {
-	employee: {
-		name: {
-			first: string;
-			last: string;
-		};
-		email: string;
-		picture: {
-			medium: string;
-		};
-	};
+  employee: {
+    name: {
+      first: string;
+      last: string;
+    };
+    email: string;
+    picture: {
+      medium: string;
+    };
+  };
 }
 
 function EmployeeCard({ employee }: EmployeeCardProps) {
-	return (
-		<figure className="DisplayEmployee">
-			<img src={employee.picture.medium} alt={employee.name.first} />
-			<figcaption>
-				<strong>
-					{employee.name.first} {employee.name.last}
-				</strong>
-				{employee.email}
-			</figcaption>
-		</figure>
-	);
+  console.log(employee);
+  return (
+    <figure className="DisplayEmployee">
+      <img src={employee.picture.medium} alt={employee.name.first} />
+      <figcaption>
+        <strong>
+          {employee.name.first} {employee.name.last}
+        </strong>
+        {employee.email}
+      </figcaption>
+    </figure>
+  );
 }
 
 export default EmployeeCard;
